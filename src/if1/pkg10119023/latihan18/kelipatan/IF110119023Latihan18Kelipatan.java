01283cefbb74e0d77a60a3c5785818baa444b276
Muhammad Farhan R <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-26 19:27:30 +0700

    Tugas 18 - Kelipatan

---
+++ src/if1/pkg10119023/latihan18/kelipatan/IF110119023Latihan18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package if1.pkg10119023.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA              : Muhammad Farhan R<farhan.10119023@mahasiswa.unikom.ac.id>
+ * KELAS             : IF1
+ * NIM               : 10119023
+ * Deskripsi Program :
+ *
+ */
+public class IF110119023Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f = 3.5f;
+        while (f <= 35){
+            System.out.println(f);
+            f = f + 3.5f;
+        }
+    }
+
+}
